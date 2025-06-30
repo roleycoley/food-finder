6de08fc738b9788b6f4b602a0e7c23427c86773c
roleycoley <cole.dungca3@gmail.com>
2025-06-29 23:18:14 -0700

    updated package.json

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
     // url of backend server (in server.js)
-    baseURL: "https://food-finder-p5j8.onrender.com/api/v1",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1/restaurants",
     headers: {
         "Content-type": "application/json"
     }
